181eb43d27901e35a0f25f7d13ac6bd9b9437222
neverorfrog <97flavio.maiorana@gmail.com>
2024-05-20 02:57:36 +0200

    build script changed

--- scripts/build_executorch.sh
+++ scripts/build_executorch.sh
@@ -4,7 +4,7 @@
 cd ${TORCH_PATH}/executorch
 
 # Remove existing cmake-out directory
-rm -rf cmake-out
+sudo rm -rf cmake-out
 
 # Create cmake-out directory
 mkdir cmake-out
@@ -17,8 +17,9 @@ cmake .. -DCMAKE_BUILD_TYPE=Release \
     -DEXECUTORCH_BUILD_SDK=ON \
     -DBUCK2=${TORCH_PATH}/buck2e \
     -DEXECUTORCH_ENABLE_LOGGING=1 \
-    -DPYTHON_EXECUTABLE=python \
-    -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON
+    -DPYTHON_EXECUTABLE=python3 \
+    -DEXECUTORCH_BUILD_EXTENSION_MODULE=ON \
+    -DEXECUTORCH_BUILD_EXTENSION_RUNNER_UTIL=ON
 
 # Check if CMake configuration was successful
 if [ $? -eq 0 ]; then
