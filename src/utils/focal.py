9ed05e133b5da22597a49f4d864acb5064aa576c
neverorfrog <97flavio.maiorana@gmail.com>
2024-07-08 11:31:22 +0200

    focal loss

--- src/utils/focal.py
+++ src/utils/focal.py
@@ -5,39 +5,27 @@ from torch.autograd import Variable
 
 
 class FocalLoss(nn.Module):
+    """
+    Wrapper for the Focal Loss function.
+    We suppose in this implementation that the input is already sigmoided.
+    """
+
     def __init__(self, gamma=0, alpha=None, size_average=True):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
-        if isinstance(alpha, (float, int)):
-            self.alpha = torch.Tensor([alpha, 1 - alpha])
-        if isinstance(alpha, list):
-            self.alpha = torch.Tensor(alpha)
         self.size_average = size_average
 
-    def forward(self, input, target):
-        if input.dim() > 2:
-            input = input.view(
-                input.size(0), input.size(1), -1
-            )  # N,C,H,W => N,C,H*W
-            input = input.transpose(1, 2)  # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(
-                -1, input.size(2)
-            )  # N,H*W,C => N*H*W,C
-        target = target.view(-1, 1)
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor):
 
-        logpt = F.log_softmax(input)
-        logpt = logpt.gather(1, target)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
+        inputs = inputs.float()
+        targets = targets.view(*inputs.shape).float()
 
-        if self.alpha is not None:
-            if self.alpha.type() != input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0, target.data.view(-1))
-            logpt = logpt * Variable(at)
+        bce_loss = nn.BCELoss(reduction="none")(inputs, targets)
 
-        loss = -1 * (1 - pt) ** self.gamma * logpt
+        pt = torch.exp(-bce_loss)
+        loss = self.alpha * (1 - pt) ** self.gamma * bce_loss
+
         if self.size_average:
             return loss.mean()
         else:
