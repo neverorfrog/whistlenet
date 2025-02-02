f914c0881af1167462f397fa6cd35c22fde34200
neverorfrog <97flavio.maiorana@gmail.com>
2025-02-02 19:06:28 +0100

    still wrong attribute

--- whistlenet/core/lightning_trainer.py
+++ whistlenet/core/lightning_trainer.py
@@ -61,7 +61,7 @@ class LightningTrainer(L.Trainer):  # type: ignore[misc]
         self, model: L.LightningModule, datamodule: L.LightningDataModule
     ) -> None:
         """Test the model on the test set."""
-        best_ckpt_path = self._checkpoint_callback.dirpath
+        best_ckpt_path = self._checkpoint_callback.filepath
 
         if not best_ckpt_path:
             raise ValueError(
